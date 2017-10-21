7db034ecfa16e3d41d6dae066fa36eb4097a4b52
Iryna Maslova <maslovai@hotmail.com>
2017-10-21 09:37:37 -0700

    working on data file

--- lab09-iryna/lib/response.js
+++ lab09-iryna/lib/response.js
@@ -2,11 +2,12 @@
 
 module.exports = {
 
-  status: (res, status, data) => {
+  sendStatus: (res, status, data) => {
     res.writeHead(status);
     res.end(data);
   },
   sendjson: (res, status, data) => {
+    console.log(data);
     res.writeHead(status, {"Content-Type":"application/json"});
     res.end(JSON.stringify(data));
   }
--- lab09-iryna/lib/route/note-router.js
+++ lab09-iryna/lib/route/note-router.js
@@ -2,36 +2,56 @@
 const Note = require('../note.js');
 const router = require('./router.js');
 const response = require('../response.js');
-
-let notes = [];
+const fs = require('fs-extra');
+const dbfile = __dirname + "/../data/notes.dat";
 
 router.get('/api/notes',(req, res) => {
+
   let id = req.url && req.url.query && req.url.query.id;
   if(id){
-    let note = notes.filter((note) => note.id === id);
-    if (note) response.sendjson(res, 200, note)
-    else response.status(res, 404, "invalid id")
+    console.log('IF');
+    fs.readJson(dbfile)
+    .then(allNotes => {
+      let note = allNotes[id];
+      response.sendjson(res, 200, note);
+    })
+    .catch( err => response.sendStatus(res, 404, err) )
   }
-  else{
-    let allNotes = {notes:notes};
-    response.sendjson(res, 200, allNotes)
+  else {
+    console.log('ELSE')
+    fs.readJson(dbfile)
+    .then(allNotes => {
+      console.log('THEN');
+      response.sendjson(res, 200, allNotes) })
+    .catch( err => {
+
+      console.log('CATCH', err);
+      response.sendStatus(res, 404, err)} )
   }
 });
 
 router.post('/api/notes',(req, res) =>{
-    if (!req.body) return response.status(res, 400, "bad request");
-    if(!req.body.title) return response.status(res, 400, "Missing Title");
-    if(!req.body.content) return response.status(res, 400, "Missing Content");
+    if (!req.body) return response.sendStatus(res, 400, "bad request");
+    if(!req.body.title) return response.sendStatus(res, 400, "Missing Title");
+    if(!req.body.content) return response.sendStatus(res, 400, "Missing Content");
+
     let note = new Note(req.body);
-    notes.push(note);
-    console.log("all notes: ",notes);
-    response.sendjson(res, 200, note)
+    let data = {};
+
+     data[note.id] = note;
+
+    let saveNote = JSON.stringify(data);
+
+    fs.outputFile(dbfile, note)
+    .then(response.sendjson(res, 200, saveNote))
+    .catch(err => response.sendStatus(res, 500, err));
+
 });
 
 router.put('/api/notes',(req, res) =>{
-  if(!req.body.title) return response.status(res, 400, "Missing Title");
-  if(!req.body.content) return response.status(res, 400, "Missing Content");
-  let id = req.url && req.url.query && req.url.query.id;
+  if(!req.body.title) return response.sendStatus(res, 400, "Missing Title");
+  if(!req.body.content) return response.sendStatus(res, 400, "Missing Content");
+   let id = req.url && req.url.query && req.url.query.id;
   if(id){
     let index = notes.indexOf(notes.id);
     notes.splice(index, 1);
@@ -46,8 +66,8 @@ router.patch('/api/notes',(req, res) => {
   let found = false;
   let id = req.url && req.url.query && req.url.query.id;
   if(id){
-    if(!req.body.title) return response.status(res, 400, "Missing Title");
-    if(!req.body.content) return response.status(res, 400, "Missing Content");
+    if(!req.body.title) return response.sendStatus(res, 400, "Missing Title");
+    if(!req.body.content) return response.sendStatus(res, 400, "Missing Content");
     for (var index = 0; index<notes.length; index++){
       if (notes[index].id === id){
         let found = true;
@@ -55,10 +75,9 @@ router.patch('/api/notes',(req, res) => {
         notes[index].content = req.body.content;
         response.sendjson(res, 200, `patched note: ${notes[index]}`);
       }
-      if (!found) response.status(res, 400, 'id not found')
     }
+    if (found===false) response.sendStatus(res, 400, 'id not found')
   }
-
 });
 
 router.delete('/api/notes',(req, res) => {
@@ -68,6 +87,6 @@ router.delete('/api/notes',(req, res) => {
       console.log("all notes: ",notes);
           response.sendjson(res, 200, `note ${id} deleted`)
     }else{
-      response.status(res, 400, 'id not found')
+      response.sendStatus(res, 400, 'id not found')
     }
 })
