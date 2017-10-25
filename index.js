87a61cb0427470a4e83710bb2df58e44587e49ec
David Boucher <david.boucher@ookla.com>
2017-10-24 19:35:28 -0700

    Pulled Lab 8 into 9

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const Storage = require('./data/storage.js');
 let Note = require('./data/note.js');
 
 let storage = new Storage();
+
+'use strict';
+
+require("dotenv").config();
+
+require('./lib/server.js').start(process.argv[2] || process.env.PORT || 3000)
+    .then(serverReply => console.log(serverReply))
+    .catch(serverError => console.log('Server error: ', serverError));
