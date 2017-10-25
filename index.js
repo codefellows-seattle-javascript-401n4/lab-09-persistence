3fe104cf4843a68d49fc981e40bba8d02fa8e2fd
David Boucher <david.boucher@ookla.com>
2017-10-24 19:27:04 -0700

    Finished storage ... for now

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ const Storage = require('./data/storage.js');
 let Note = require('./data/note.js');
 
 let storage = new Storage();
-
-let newNote = new Note('Note contentssss');
- 
-storage.deleteNote(newNote).then(console.log);
